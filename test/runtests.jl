84a530b5ef608099304008ffd010e070e80224e8
WhiffleFish <tylertronics@gmail.com>
2023-04-26 19:21:11 -0600

    add simple test env

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,18 @@
-using ConstrainedPOMDPModels
+using Pkg
+Pkg.add(url="https://github.com/qhho/ConstrainedPOMDPs.jl")
 
-ConstrainedGridWorldPOMDP()
+using ConstrainedPOMDPModels
+using ConstrainedPOMDPs
+using POMDPTools
+using POMDPs
+using Test
+
+@testset "gridworld" begin
+    pomdp = ConstrainedGridWorldPOMDP()
+    @test has_consistent_distributions(pomdp)
+    
+    pomdp = ConstrainedGridWorldPOMDP(obs_prob = 1.0)
+    @test has_consistent_distributions(pomdp)
+end
+
+
